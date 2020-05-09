7f543730270ef438483ecc5064e9a20d501316b1
John Allison <john123allison@gmail.com>
2020-05-09 12:58:30 -0700

    fixed speling

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 # get oh my zsh
+sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cp .vim ~/.vim -rf
-cp .vimrc ~/,vimrc
+cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
 
 vim +PlugInstall
