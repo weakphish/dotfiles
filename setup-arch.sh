29148ec6907f5116332131871fdb736922520a18
John Allison <john123allison@gmail.com>
2020-07-14 14:58:53 -0400

    added shell greeting, piped yes into repo installs

--- setup-arch.sh
+++ setup-arch.sh
@@ -1,7 +1,12 @@
 # get oh my zsh
-sudo pacman -S zsh
-sudo pacman -S curl
-sudo pacman -S ttf-fira-code
+yes | sudo pacman -S zsh
+yes | sudo pacman -S curl
+yes | sudo pacman -S ttf-fira-code
+yes | sudo pacman -S fortune
+yes | sudo pacman -S cowsay
+yes | sudo pacman -S lolcat
+yes | sudo pacman -S neofetch
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cp .vim ~/.vim -rf
--- setup-deb.sh
+++ setup-deb.sh
@@ -1,6 +1,11 @@
 # get oh my zsh
-sudo apt install zsh
-sudo apt install curl
+yes | sudo apt install zsh
+yes | sudo apt install curl
+yes | sudo apt install fortune 
+yes | sudo apt install lolcat
+yes | sudo apt install neofetch
+yes | sudo apt install cowsay
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cp .vim ~/.vim -rf
