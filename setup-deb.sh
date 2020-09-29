cb0db2cb731744455a190506ae72fe26893aff28
John Allison <jeallis2@ncsu.edu>
2020-09-29 14:24:17 -0400

    added GNOME configs

--- setup-deb.sh
+++ setup-deb.sh
@@ -1,10 +1,16 @@
 # Get basic tools
 yes | sudo apt install zsh curl fortune lolcat cowsay neofetch neovim
 
+# Copy configs
+cp i3 ~/.config/i3
+cp i3status ~/.config/i3status
+cp .icons ~/
+cp .themes ~/
+mkdir ~/.config/nvim/
+cp init.vim ~/.config/nvim/init.vim
+
 # Install neovim plugin manager
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# Copy NVIM config 
-cp init.vim ~/.config/nvim/init.vim
 
 # Copy zsh config over
 cp .zshrc ~/.zshrc
