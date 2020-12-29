c1a654c182e1c272b3e43a13891a92fa516fc8fa
John <john123allison@gmail.com>
2020-12-28 19:55:06 -0500

    update emacs config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -19,7 +19,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Fira Code" :size 14 :weight 'regular)
+(setq doom-font (font-spec :family "Jetbrains Mono" :size 14 :weight 'regular)
        doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
@@ -29,11 +29,12 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Dropbox/Notes")
+(setq org-directory "~/Dropbox/Notes/")
+(setq org-agenda-files "~/Dropbox/Notes/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 
 
 ;; Here are some additional functions/macros that could help you configure Doom:
