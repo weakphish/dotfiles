dfcedc19e525fd13ea46b03afe808151d600863c
John Allison <john123allison@gmail.com>
2021-01-27 15:17:10 -0500

    clean repo

--- setup.sh
+++ setup.sh
@@ -5,17 +5,14 @@ yes | sudo apt install zsh curl fortune lolcat neofetch neovim
 mkdir ~/.config/nvim/
 cp init.vim ~/.config/nvim/init.vim
 
+cp ./.tmux.conf ~/
+
 # Install neovim plugin manager
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Emacs
-yes | sudo apt install emacs
-git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-~/.emacs.d/bin/doom install
-
-cp ./.doom.d/* ~/.doom.d/*
-
-# Copy zsh config over
-cp .zshrc ~/.zshrc
 # Get oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Copy zsh config over
+cp ./.zshrc ~/.zshrc
+
