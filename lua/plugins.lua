1e9ce57d371b17beba96d993fc8071273cbfbdef
John <john123allison@gmail.com>
2022-01-05 17:09:08 -0500

    add native LSP

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,19 @@
 return require('packer').startup(function()
+    -- Packer manages itself, lol
+    use 'wbthomason/packer.nvim'
     -- IDE-Features
-    use 'preservim/nerdtree'
-    use 'preservim/tagbar'
+    use 'preservim/nerdtree' -- Filetree
+    use 'preservim/tagbar' -- Outline of file
     use 'andymass/vim-matchup'
-    use 'godlygeek/tabular'
-    use 'tpope/vim-fugitive'
-    use 'nvim-lua/plenary.nvim'
-    use 'nvim-telescope/telescope.nvim'
+    use 'godlygeek/tabular' -- Lining up text
+    use 'tpope/vim-fugitive' -- Git stuff
+    use 'nvim-telescope/telescope.nvim' -- Nice grep interface
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use 'neovim/nvim-lspconfig' -- LSP configurations
+    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
     -- Languages
     use 'sheerun/vim-polyglot'
     use 'rust-lang/rust.vim'
