419a832f090ffe696a1f217086d8846d6f6a2632
John <john123allison@gmail.com>
2022-01-09 18:00:04 -0500

    add lsp updater

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,6 +11,7 @@ return require('packer').startup(function()
     use 'nvim-telescope/telescope.nvim' -- Nice grep interface
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use 'neovim/nvim-lspconfig' -- LSP configurations
+    use 'alexaandru/nvim-lspupdate' -- Autoinstall LSP servers
     use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
     use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
     use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
