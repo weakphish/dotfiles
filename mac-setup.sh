2e31f07695d434f7aa403726f318dcb735ecbd20
John Allison <john123allison@gmail.com>
2022-04-04 02:15:41 -0400

    refactor mac setup script

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,30 +1,25 @@
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Copy vim config
-cp vimrc ~/.vimrc
+# install neovim
+brew install neovim
 
-# Copy VS Code config (FIXME assumes the correct path which should be fixed)
-cp ./vs-code/* /Users/weakphish/Library/Application\ Support/Code/User/
-
-# Install NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash &
-
-# Install GVM
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer) &
+# install neovim config
+mkdir -p ~/.config/neovim/
+cp neovim/* ~/.config/neovim/
 
 # install fish and adjacent items
 brew install fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install ilancosman/tide
+omf install lambda
 
 # Install Bass
 omf install bass
 
 # Copy fish functions
 mkdir -p ~/.config/fish/functions
-cp ./fish/* ~/.config/fish/functions
+cp ./fish/config.fish ~/.config/fish/
+cp ./fish/* ~/.config/fish/functions/
 
 # Install The Good Font
 brew install font-jetbrains-mono-nerd-font
