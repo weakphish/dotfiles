7309035ebc560ccc3d25e29b7d2706f8a10651fe
John Allison <john123allison@gmail.com>
2022-04-19 20:39:04 -0400

    update mac setup script

--- mac-setup.sh
+++ mac-setup.sh
@@ -11,10 +11,10 @@ curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 # install Bass
 fisher install bass
 
-# copy fish functions
+# copy fish config and functions
 mkdir -p ~/.config/fish/functions
 cp ./fish/config.fish ~/.config/fish/
-cp ./fish/* ~/.config/fish/functions/
+cp ./fish/functions/* ~/.config/fish/functions/
 
 # install tide prompt
 fisher install hasanozgan/theme-lambda
