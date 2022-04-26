2be63fc16ac31c49873f6126032c43d08c64b18d
John Allison <john123allison@gmail.com>
2022-04-25 20:53:23 -0400

    update mac setup script

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,16 +1,6 @@
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Install emacs (doom)
-brew tap d12frosted/emacs-plus
-brew install emacs-plus@28
-
-git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-~/.emacs.d/bin/doom install
-
-# copy doom config over
-cp ./emacs/* ~/.doom.d/
-
 # install neovim
 brew install neovim
 
