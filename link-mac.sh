4250d4befd02e72c11db231c5148a700f733413f
John Allison <john123allison@gmail.com>
2022-06-24 21:22:38 -0400

    add doom emacs

--- link-mac.sh
+++ link-mac.sh
@@ -3,11 +3,14 @@
 # Bare Vim
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
-# Neovim
-ln -s ~/.dotfiles/nvim ~/.config/nvim
+# Doom Emacs
+ln -s ~/.dotfiles/.doom.d ~/.doom.d
 
 # Fish
 ln -s ~/.dotfiles/fish ~/.config/fish
 
 # Helix
 ln -s ~/.dotfiles/helix ~/.config/helix
+
+# Neovim
+ln -s ~/.dotfiles/nvim ~/.config/nvim
