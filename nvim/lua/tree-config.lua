5910fadf456bcf72da03f548d668c3c5b3650e19
John Allison <john123allison@gmail.com>
2022-07-07 17:18:13 -0400

    update nvimtree keybinds

--- nvim/lua/tree-config.lua
+++ nvim/lua/tree-config.lua
@@ -4,8 +4,14 @@ require("nvim-tree").setup({
   view = {
     adaptive_size = true,
     mappings = {
+      custom_only = false,
       list = {
         { key = "u", action = "dir_up" },
+        { key = "t", action = "tabnew" },
+        { key = "e", action = "edit", action_cb = edit_or_open },
+        { key = "v", action = "vsplit", action_cb = vsplit },
+        { key = "h", action = "close_node" },
+        { key = "H", action = "collapse_all", action_cb = collapse_all }
       },
     },
   },
