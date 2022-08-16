d6d02bf19a8d5a4877e1336b37f9941153067b9d
Jack Allison <jacallis@cisco.com>
2022-08-16 12:59:25 -0400

    make completion work better

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,14 +1,14 @@
 return require('packer').startup(function()
-    use 'wbthomason/packer.nvim'        -- Packer manages itself, lol
-    use 'lewis6991/impatient.nvim'      -- Improve startup time
+    use 'wbthomason/packer.nvim'                -- Packer manages itself, lol
+    use 'lewis6991/impatient.nvim'              -- Improve startup time
     -- IDE-Features
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } -- Make highlighting based on an AST, no Regex
-    use {'stevearc/aerial.nvim'}        -- Show outline of file's symbols
-    use 'tpope/vim-fugitive'            -- Git inside NeoVim
-    use 'nvim-lua/plenary.nvim'         -- Required by Telescope
-    use 'nvim-telescope/telescope.nvim' -- Nice interface for various menus (mostly grep)
+    use {'stevearc/aerial.nvim'}                -- Show outline of file's symbols
+    use 'tpope/vim-fugitive'                    -- Git inside NeoVim
+    use 'nvim-lua/plenary.nvim'                 -- Required by Telescope
+    use 'nvim-telescope/telescope.nvim'         -- Nice interface for various menus (mostly grep)
     use {'nvim-telescope/telescope-ui-select.nvim' } -- And, use telescope for more default stuff
-    use {                               -- Nicer installation for LSP servers
+    use {                                       -- Nicer installation for LSP servers
     'junnplus/lsp-setup.nvim',
     requires = {
         'neovim/nvim-lspconfig',
@@ -16,36 +16,37 @@ return require('packer').startup(function()
         'williamboman/mason-lspconfig.nvim',
     }
 }
-    use 'hrsh7th/nvim-cmp'              -- Autocompletion plugin
-    use 'hrsh7th/vim-vsnip'             -- Dependency for above features
-    use 'hrsh7th/cmp-nvim-lsp'          -- LSP source for nvim-cmp
-    use 'saadparwaiz1/cmp_luasnip'      -- Snippets source for nvim-cmp
-    use 'L3MON4D3/LuaSnip'              -- Snippets plugin
-    use {                               -- Pop-up of available commands when working bindings
+    use 'hrsh7th/nvim-cmp'                      -- Autocompletion plugin
+    use 'hrsh7th/vim-vsnip'                     -- Dependency for above features
+    use 'hrsh7th/cmp-nvim-lsp'                  -- LSP source for nvim-cmp
+    use 'hrsh7th/cmp-nvim-lsp-signature-help'   -- show function details while typing
+    use 'saadparwaiz1/cmp_luasnip'              -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip'                      -- Snippets plugin
+    use {                                       -- Pop-up of available commands when working bindings
       "folke/which-key.nvim",
       config = function()
         require("which-key").setup {}
       end
     }
     -- Languages
-    use 'sheerun/vim-polyglot'          -- Works for most languages
-    use {'rust-lang/rust.vim', ft='.rs'}-- More specific stuff for Rust
-    use 'simrat39/rust-tools.nvim'      -- ...and even more...
-    use 'p00f/clangd_extensions.nvim'   -- clangd extensions for C LSP
-    use 'fatih/vim-go'                  -- Specific stuff for Go
-    use 'dart-lang/dart-vim-plugin'     -- Dart support
+    use 'sheerun/vim-polyglot'                  -- Works for most languages
+    use {'rust-lang/rust.vim', ft='.rs'}        -- More specific stuff for Rust
+    use 'simrat39/rust-tools.nvim'              -- ...and even more...
+    use 'p00f/clangd_extensions.nvim'           -- clangd extensions for C LSP
+    use 'fatih/vim-go'                          -- Specific stuff for Go
+    use 'dart-lang/dart-vim-plugin'             -- Dart support
     -- Aesthetic/Appearance
-    use {                               -- File tree
+    use {                                       -- File tree
       'kyazdani42/nvim-tree.lua',
       requires = {
-        'kyazdani42/nvim-web-devicons', -- optional, for file icons
+        'kyazdani42/nvim-web-devicons',         -- optional, for file icons
       },
-      tag = 'nightly'                   -- optional, updated every week. (see issue #1193)
+      tag = 'nightly'                           -- optional, updated every week. (see issue #1193)
     }
-    use 'sainnhe/gruvbox-material'      -- A refresh on a classic
-    use 'sainnhe/sonokai'               -- Monokai Pro, but free
-    use 'nvim-lualine/lualine.nvim'     -- Statusbar go brrrr
-    use "lukas-reineke/indent-blankline.nvim" -- Indent guides
-    use {'stevearc/dressing.nvim'}      -- Make things generally prettier
-    use 'airblade/vim-gitgutter'        -- Git gutter indicators
+    use 'sainnhe/gruvbox-material'              -- A refresh on a classic
+    use 'sainnhe/sonokai'                       -- Monokai Pro, but free
+    use 'nvim-lualine/lualine.nvim'             -- Statusbar go brrrr
+    use "lukas-reineke/indent-blankline.nvim"   -- Indent guides
+    use {'stevearc/dressing.nvim'}              -- Make things generally prettier
+    use 'airblade/vim-gitgutter'                -- Git gutter indicators
 end)
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -89,6 +89,11 @@ _G.packer_plugins = {
     path = "/Users/jacallis/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
+  ["cmp-nvim-lsp-signature-help"] = {
+    loaded = true,
+    path = "/Users/jacallis/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp-signature-help",
+    url = "https://github.com/hrsh7th/cmp-nvim-lsp-signature-help"
+  },
   cmp_luasnip = {
     loaded = true,
     path = "/Users/jacallis/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
