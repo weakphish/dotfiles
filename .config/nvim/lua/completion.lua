826a69263243c9855ca4bdabdef426ba595e094a
Jack Allison <jacallis@cisco.com>
2022-09-29 15:38:41 -0400

    chore: tidy

--- .config/nvim/lua/completion.lua
+++ .config/nvim/lua/completion.lua
@@ -1,14 +1,6 @@
 require('lsp-setup').setup({
     installer = {},
     default_mappings = false,
---    mappings = {
---        ['<leader>gD']= 'lua vim.lsp.buf.declaration()',
---        ['<leader>gd'] = 'lua vim.lsp.buf.definition()',
---        ['<leader>k'] = 'lua vim.lsp.buf.hover()',
---        ['<leader>cr'] = 'lua vim.lsp.buf.rename()',
---        ['<leader>ca'] = 'lua vim.lsp.buf.code_action()',
---        ['<leader>cf'] = 'lua vim.lsp.buf.formatting()',
---    },
     -- Global on_attach
     on_attach = function(client, bufnr) 
         require('lsp-setup.utils').format_on_save(client)
@@ -29,7 +21,8 @@ require('lsp-setup').setup({
     end,
     -- Global capabilities
     capabilities = vim.lsp.protocol.make_client_capabilities(),
-    -- LSP servers
+
+    -- LSP servers --
     servers = {
         -- Install LSP servers automatically
         -- LSP server configuration please see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
