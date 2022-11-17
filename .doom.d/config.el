e045d261c92c2feee75dcf1ab613e4c91b70dac2
Jack Allison <jacallis@cisco.com>
2022-11-17 11:26:16 -0500

    IBM Plex Mono / Sans

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Iosevka" :size 13 :weight 'normal)
-      doom-variable-pitch-font (font-spec :family "Iosevka" :size 12))
+(setq doom-font (font-spec :family "IBM Plex Mono" :size 13 :weight 'normal)
+      doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 12))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
