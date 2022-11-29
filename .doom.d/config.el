728cea9712dfdf9ed379a5ae0836d497f89ef0bc
Jack Allison <jacallis@cisco.com>
2022-11-29 12:03:27 -0500

    Doom-one

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "IBM Plex Mono" :size 13 :weight 'normal)
-      doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 12))
+(setq doom-font (font-spec :family "JetBrains Mono" :size 12 :weight 'normal)
+      doom-variable-pitch-font (font-spec :family "JetBrains Mono" :size 12))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'doom-one)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
