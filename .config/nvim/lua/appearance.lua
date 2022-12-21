9f46b0464d8e93decc59c98e860980288c3a17a5
Jack Allison <jacallis@cisco.com>
2022-12-21 14:47:13 -0500

    Gruvbox emacs

--- .config/nvim/lua/appearance.lua
+++ .config/nvim/lua/appearance.lua
@@ -14,11 +14,10 @@ vim.opt.termguicolors = true
 -- Sonokai preferences
 vim.g.sonokai_better_performance = 1
 vim.g.sonokai_style = "shusia"
-vim.g.vscode_style = "dark"
 
 -- Set colorscheme
 vim.cmd [[
-  colorscheme sonokai
+  colorscheme gruvbox-material
 ]]
 
 -- Add extra Go highlights
