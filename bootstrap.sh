23df7dd3c7dfd32d92c94ade8290fc884c646996
John Allison <john123allison@gmail.com>
2023-02-09 12:13:48 -0500

    fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,20 @@
 # Bootstrap homebrew and fish shell
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install fish
+fish
+fish_add_path /opt/homebrew/bin
+echo "/opt/homebrew/bin/fish" | sudo tee -a /etc/shells
+chsh -s /opt/homebrew/bin/fish
+
+
+# This script assumes that fish-shell is installed via Homebrew and is set
+# as the default shell.
+
+# install omf
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+# install fzf
+brew install fzf
+
+# install helix
+brew install helix
