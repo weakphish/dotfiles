44b98da3486359cb9bf1a70a3761bbc40082c09e
John Allison <john123allison@gmail.com>
2023-03-05 13:48:56 -0500

    Adapt parts of Tecosaur's config + add some cool stuff

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -21,8 +21,10 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "JetBrains Mono" :size 12 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "Helvetica Neue" :size 13))
+(setq doom-font (font-spec :family "Fira Code" :size 12)
+      doom-big-font (font-spec :family "Fira Code" :size 26)
+      doom-variable-pitch-font (font-spec :family "SF Pro" :size 13)
+      doom-serif-font (font-spec :family "New York" :size 13 :weight 'light))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -78,3 +80,95 @@
 ;; Group centaur tabs by project
 (after! centaur-tabs
         (centaur-tabs-group-by-projectile-project))
+
+;; Add a fortune to our splash screen. Adapted from Tecosaur's literate config.
+;; https://github.com/tecosaur/emacs-config/blob/master/config.org#org-development-profile
+(defun splash-phrase-dashboard-formatted ()
+  "Get a splash phrase, flow it over multiple lines as needed, and fontify it."
+  (mapconcat
+   (lambda (line)
+     (+doom-dashboard--center
+      +doom-dashboard--width
+      (with-temp-buffer
+        (insert-text-button
+         line
+         'action
+         (lambda (_) (+doom-dashboard-reload t))
+         'face 'doom-dashboard-menu-title
+         'mouse-face 'doom-dashboard-menu-title
+         'help-echo "Random phrase"
+         'follow-link t)
+        (buffer-string))))
+   (split-string
+    (with-temp-buffer
+      (insert (shell-command-to-string "fortune"))
+      (setq fill-column (min 70 (/ (* 2 (window-width)) 3)))
+      (fill-region (point-min) (point-max))
+      (buffer-string))
+    "\n")
+   "\n"))
+(defun splash-phrase-dashboard-insert ()
+  "Insert the splash phrase surrounded by newlines."
+  (insert "\n" (splash-phrase-dashboard-formatted) "\n"))
+
+;; More Tecosaur - add some stuff to the Doom dashboard
+;; When using the dashboard, there are often a small number of actions I will take.
+;; As the dashboard is it’s own major mode, there is no need to suffer the tyranny of
+;; unnecessary keystrokes — we can simply bind common actions to a single key!
+(defun +doom-dashboard-setup-modified-keymap ()
+  (setq +doom-dashboard-mode-map (make-sparse-keymap))
+  (map! :map +doom-dashboard-mode-map
+        :desc "Find file" :ng "f" #'find-file
+        :desc "Config dir" :ng "C" #'doom/open-private-config
+        :desc "Open dotfiles" :ng "." (cmd! (doom-project-find-file "~/.dotfiles/"))
+        :desc "IBuffer" :ng "i" #'ibuffer
+        :desc "Set theme" :ng "t" #'consult-theme
+        :desc "Quit" :ng "Q" #'save-buffers-kill-emacs
+        :desc "Show keybindings" :ng "h" (cmd! (which-key-show-keymap '+doom-dashboard-mode-map))))
+
+(add-transient-hook! #'+doom-dashboard-mode (+doom-dashboard-setup-modified-keymap))
+(add-transient-hook! #'+doom-dashboard-mode :append (+doom-dashboard-setup-modified-keymap))
+(add-hook! 'doom-init-ui-hook :append (+doom-dashboard-setup-modified-keymap))
+;; Unfortunately the show keybindings help doesn’t currently work as intended, but this is still quite nice overall.
+;; Now that the dashboard is so convenient, I’ll want to make it easier to get to.
+(map! :leader :desc "Dashboard" "d" #'+doom-dashboard/open)
+;; Put it all together
+;; With the splash image and phrase generation worked out, we can almost put together the desired
+;; dashboard from scratch, we just need to re-create the benchmark information by itself.
+(defun +doom-dashboard-benchmark-line ()
+  "Insert the load time line."
+  (when doom-init-time
+    (insert
+     "\n\n"
+     (propertize
+      (+doom-dashboard--center
+       +doom-dashboard--width
+       (doom-display-benchmark-h 'return))
+      'face 'doom-dashboard-loaded))))
+;; With doom-display-benchmark-h displayed here, I don’t see the need for it to be shown in the minibuffer as well.
+(remove-hook 'doom-after-init-hook #'doom-display-benchmark-h)
+
+;; Now we can create the desired dashboard by setting +doom-dashboard-functions to just have:
+;; - The “widget banner” (splash image)
+;; - The benchmark line
+;; - A random phrase
+;; This gets rid of two segments I’m not particularly interested in seeing
+;; - The shortmenu
+;; - The footer (github link)
+(setq +doom-dashboard-functions
+      (list #'doom-dashboard-widget-banner
+            #'+doom-dashboard-benchmark-line
+            #'splash-phrase-dashboard-insert))
+;; At this point there are just a few minor tweaks I’d still like to make to the dashboard.
+(defun +doom-dashboard-tweak (&optional _)
+  (with-current-buffer (get-buffer +doom-dashboard-name)
+    (setq-local line-spacing 0.2
+                mode-line-format nil
+                evil-normal-state-cursor (list nil))))
+;; Now we can just add this as a mode hook.
+(add-hook '+doom-dashboard-mode-hook #'+doom-dashboard-tweak)
+;; (add-hook 'doom-after-init-hook #'+doom-dashboard-tweak 1)
+;; Lastly, with the buffer name being shown in the frame title thanks to some other configuration,
+;; we might as well display something a bit prettier than *doom*.
+(setq +doom-dashboard-name "► Doom"
+      doom-fallback-buffer-name +doom-dashboard-name)
--- .config/doom/init.el
+++ .config/doom/init.el
@@ -21,11 +21,11 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       (company +childframe)          ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ;;ivy               ; a search engine for love and life
-       vertico           ; the search engine of the future
+       (ivy +childframe +icons +fuzzy)              ; a search engine for love and life
+       ;; vertico           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
@@ -116,7 +116,7 @@
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        ;;(cc +lsp)         ; C > C++ == 1
-       ;;clojure           ; java with a lisp
+       (clojure +lsp)           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
@@ -153,7 +153,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +jupyter +pandoc +pretty)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
