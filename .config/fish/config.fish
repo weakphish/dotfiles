69e3fce5b28de153fabd6c84f68021ebfecf8646
Jack Allison <jacallis@cisco.com>
2023-07-07 13:55:34 -0400

    set editor to helix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,7 @@ if status is-interactive
     fish_vi_key_bindings
 end
 
-set -Ux EDITOR nvim
+set -Ux EDITOR hx
 alias lg lazygit
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
