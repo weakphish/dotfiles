2257bc43b510ab394a2cbd1784e8ae8727182799
Jack Allison <jacallis@cisco.com>
2023-07-17 12:28:47 -0400

    copilot in neovim thanks work

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -347,6 +347,10 @@ else
 
     {
       'github/copilot.vim',
+      config = function()
+        vim.g.copilot_no_tab_map = true
+        vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      end,
     },
 
     -- NOTE: === VISUAL / AESTHETIC ---
