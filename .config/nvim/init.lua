4ef21e03408427daf0e59df6eb1d9b0356df3262
Jack Allison <jacallis@cisco.com>
2023-07-18 11:43:54 -0400

    setup Noice for cute UI

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -355,6 +355,49 @@ else
 
     -- NOTE: === VISUAL / AESTHETIC ---
 
+    -- Highly experimental plugin that completely replaces the UI for messages, cmdline and the popupmenu.
+    {
+      'folke/noice.nvim',
+      event = 'VeryLazy',
+      opts = {
+        -- add any options here
+      },
+      dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        'MunifTanjim/nui.nvim',
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        'rcarriga/nvim-notify',
+      },
+      config = function()
+        require('noice').setup {
+          lsp = {
+            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            override = {
+              ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+              ['vim.lsp.util.stylize_markdown'] = true,
+              ['cmp.entry.get_documentation'] = true,
+            },
+          },
+          -- you can enable a preset for easier configuration
+          presets = {
+            bottom_search = true, -- use a classic bottom cmdline for search
+            command_palette = true, -- position the cmdline and popupmenu together
+            long_message_to_split = true, -- long messages will be sent to a split
+            inc_rename = false, -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = true, -- add a border to hover docs and signature help
+          },
+        }
+      end,
+    },
+
+    -- Make things generally prettier
+    {
+      'stevearc/dressing.nvim',
+      opts = {},
+    },
+
     -- Dashboard like Doom Emacs
     {
       'goolord/alpha-nvim',
@@ -379,12 +422,6 @@ else
       end,
     },
 
-    -- Better popups
-    {
-      'nvim-lua/popup.nvim',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-    },
-
     -- Pretty Icons
     'nvim-tree/nvim-web-devicons',
 
@@ -393,12 +430,6 @@ else
 
     -- Detect tabstop and shiftwidth automatically
     'tpope/vim-sleuth',
-
-    -- Make things generally prettier
-    {
-      'stevearc/dressing.nvim',
-      opts = {},
-    },
 
     {
       -- Neotree - file tree browser
@@ -611,6 +642,8 @@ else
         pcall(require('telescope').load_extension, 'dap')
         -- Enable telescope undo
         pcall(require('telescope').load_extension, 'undo')
+        -- Enable telescope with Noice
+        require('telescope').load_extension 'noice'
 
         -- [[ Configure Treesitter ]]
         -- See `:help nvim-treesitter`
