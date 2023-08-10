207f6a899698a8db77364d94c6359da8659b35a7
Jack Allison <jacallis@cisco.com>
2023-08-10 10:31:28 -0400

    remove org

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -294,7 +294,6 @@ else
           sources = {
             { name = 'nvim_lsp' },
             { name = 'luasnip' },
-            { name = 'orgmode' },
           },
         }
 
@@ -573,32 +572,6 @@ else
     },
 
     -- NOTE: === TOOLS ===
-
-    {
-      'nvim-orgmode/orgmode',
-      config = function()
-        -- Load custom treesitter grammar for org filetype
-        require('orgmode').setup_ts_grammar()
-
-        -- Treesitter configuration
-        require('nvim-treesitter.configs').setup {
-          -- If TS highlights are not enabled at all, or disabled via `disable` prop,
-          -- highlighting will fallback to default Vim syntax highlighting
-          highlight = {
-            enable = true,
-            -- Required for spellcheck, some LaTex highlights and
-            -- code block highlights that do not have ts grammar
-            additional_vim_regex_highlighting = { 'org' },
-          },
-          ensure_installed = { 'org' }, -- Or run :TSUpdate org
-        }
-
-        require('orgmode').setup {
-          org_agenda_files = { '~/org' },
-          org_default_notes_file = '~/org/notes.org',
-        }
-      end,
-    },
 
     -- Easy toggling of terminals
     { 'akinsho/toggleterm.nvim', version = '*', config = true },
