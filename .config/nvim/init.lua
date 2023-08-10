002be45ca8491973f83d17331df90433c21e9101
Jack Allison <jacallis@cisco.com>
2023-08-10 10:01:50 -0400

    org in neovim?

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,8 +9,6 @@ else
   vim.g.maplocalleader = ' '
 
   -- [[ Setting general options ]]
-  -- See `:help vim.o`
-
   -- Set highlight on search
   vim.o.hlsearch = false
 
@@ -102,7 +100,6 @@ else
 
   -- NOTE: Here is where you install your plugins.
   --  You can configure plugins using the `config` key.
-  --
   --  You can also configure plugins after the setup call,
   --    as they will be available in your neovim runtime.
   require('lazy').setup({
@@ -297,6 +294,7 @@ else
           sources = {
             { name = 'nvim_lsp' },
             { name = 'luasnip' },
+            { name = 'orgmode' },
           },
         }
 
@@ -575,6 +573,32 @@ else
     },
 
     -- NOTE: === TOOLS ===
+
+    {
+      'nvim-orgmode/orgmode',
+      config = function()
+        -- Load custom treesitter grammar for org filetype
+        require('orgmode').setup_ts_grammar()
+
+        -- Treesitter configuration
+        require('nvim-treesitter.configs').setup {
+          -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+          -- highlighting will fallback to default Vim syntax highlighting
+          highlight = {
+            enable = true,
+            -- Required for spellcheck, some LaTex highlights and
+            -- code block highlights that do not have ts grammar
+            additional_vim_regex_highlighting = { 'org' },
+          },
+          ensure_installed = { 'org' }, -- Or run :TSUpdate org
+        }
+
+        require('orgmode').setup {
+          org_agenda_files = { '~/org' },
+          org_default_notes_file = '~/org/notes.org',
+        }
+      end,
+    },
 
     -- Easy toggling of terminals
     { 'akinsho/toggleterm.nvim', version = '*', config = true },
