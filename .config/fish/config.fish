75390a0391f6d71e643885a8e23d1d69a41b1145
Jack Allison <jacallis@cisco.com>
2023-09-06 10:09:30 -0400

    vi keybinds in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,8 @@ set -Ux EDITOR hx
 alias lg lazygit
 alias tf terraform
 
+# Vim mode
+fish_key_bindings
+
 # iTerm integration
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
