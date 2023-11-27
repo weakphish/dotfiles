fa08e0ca4796a47bd167243d09a02d10e8d2c9f5
Jack Allison <jacallis@cisco.com>
2023-11-27 16:20:21 -0500

    use prettier

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -222,6 +222,7 @@ else
             null_ls.builtins.formatting.isort,
             null_ls.builtins.formatting.black,
             null_ls.builtins.diagnostics.eslint,
+            null_ls.builtins.formatting.prettier,
             -- null_ls.builtins.completion.spell,
           },
         }
