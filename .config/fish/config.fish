16c50ddaa5530874bdbc3fb0a8e42920b5337fca
Jack Allison <jacallis@cisco.com>
2023-11-28 11:03:41 -0500

    helix as nvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-set -Ux EDITOR nvim
+set -Ux EDITOR hx
 
 # Aliases
 alias lg lazygit
@@ -6,9 +6,6 @@ alias tf terraform
 alias phx "poetry run hx"
 alias pnv "poetry run nvim"
 alias dc "docker compose"
-
-# Vim mode
-fish_vi_key_bindings
 
 # iTerm integration
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
