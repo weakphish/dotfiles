0b41a102fd8a249abaacd843a53c71cd522aaa45
Jack Allison <jacallis@cisco.com>
2023-12-05 11:32:40 -0500

    Add neotest debugging for python

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -537,7 +537,14 @@ else
       config = function()
         require('neotest').setup {
           adapters = {
-            require 'neotest-python',
+            require 'neotest-python' {
+              dap = {
+                justMyCode = false,
+                console = 'integratedTerminal',
+              },
+              args = { '--log-level', 'DEBUG', '--quiet' },
+              runner = 'pytest',
+            },
           },
         }
       end,
