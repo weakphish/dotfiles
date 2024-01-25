0ebc486fcdd088b7b072faec4863109c67dfaf5b
Jack Allison <jacallis@cisco.com>
2024-01-25 16:14:20 -0500

    make obsidian lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -554,7 +554,7 @@ else
       -- Obsidian in Neovim
       'epwalsh/obsidian.nvim',
       version = '*', -- recommended, use latest release instead of latest commit
-      lazy = false,
+      lazy = true,
       ft = 'markdown',
       -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
       -- event = {
