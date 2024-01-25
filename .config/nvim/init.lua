4fcd62b68d95d01714efb297211090e668b4e01b
Jack Allison <jacallis@cisco.com>
2024-01-25 10:48:42 -0500

    remove format on save

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -322,11 +322,6 @@ else
             -- Use a sub-list to run only the first available formatter
             javascript = { { 'prettierd', 'prettier' } },
           },
-          format_on_save = {
-            -- These options will be passed to conform.format()
-            timeout_ms = 500,
-            lsp_fallback = true,
-          },
         }
       end,
     },
