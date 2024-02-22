7f7763c8155c30f338cec5ccd9e332537393aab0
Jack Allison <jacallis@cisco.com>
2024-02-22 13:21:16 -0500

    update neotree keybind 2

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -95,7 +95,7 @@ return {
         name = 'Toggle',
         a = { '<cmd>AerialToggle<CR>', 'Aerial' },
         b = { '<cmd>TroubleToggle<CR>', 'Trouble' },
-        n = { '<cmd>Neotree<CR>', 'NeoTree' },
+        n = { '<cmd>Neotree toggle<CR>', 'NeoTree' },
         t = { '<cmd>:ToggleTerm size=30<CR>', 'Toggle Terminal' },
       },
       w = {
