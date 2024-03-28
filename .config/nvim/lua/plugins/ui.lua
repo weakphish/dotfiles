bba9be25875466315fd704c347a8c97e15c5a118
Jack Allison <jacallis@cisco.com>
2024-03-28 12:47:26 -0400

    onedark

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -109,11 +109,11 @@ return {
 
   -- Pretty colors
   {
-    'projekt0n/github-nvim-theme',
+    'navarasu/onedark.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.cmd 'colorscheme github_dark_dimmed'
+      require('onedark').load()
     end,
   },
 
@@ -123,6 +123,7 @@ return {
     -- See `:help lualine.txt`
     opts = {
       options = {
+        theme = 'onedark',
         icons_enabled = false,
         component_separators = '|',
         section_separators = '',
